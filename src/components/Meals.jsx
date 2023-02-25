25f93c90f77118801d02470e293185ecd6a7cbf3
Said <slaoualy@yahoo.fr>
2023-02-25 11:59:15 +0100

    removed th console logs

--- src/components/Meals.jsx
+++ src/components/Meals.jsx
@@ -1,13 +1,11 @@
 import { useGlobalContext } from '../context';
 
 const Meals = () => {
-  // const context = useGlobalContext();
   const { meals } = useGlobalContext();
-  console.log(meals);
+
   return (
     <section>
       {meals.map((meal) => {
-        // console.log(meal);
         return <h3>{meal.strMeal}</h3>;
       })}
     </section>
