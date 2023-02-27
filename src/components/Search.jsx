61a4471f3911145a53c649f991cfe81488939148
Said <slaoualy@yahoo.fr>
2023-02-27 11:43:15 +0100

    added a placeholder to the input

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -2,11 +2,14 @@ import { useState } from 'react';
 import { useGlobalContext } from '../context';
 
 const Search = () => {
-  const { search } = useGlobalContext();
   return (
     <header className="search-container">
       <form>
-        <input className="form-input" type="text"></input>
+        <input
+          className="form-input"
+          type="text"
+          placeholder="Search a meal"
+        ></input>
         <button className="btn" type="submit">
           Search
         </button>
